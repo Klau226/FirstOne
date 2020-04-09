2ac707abaf6f0dae2a825f01d798a6fc32758df3
Klau226 <63421864+Klau226@users.noreply.github.com>
2020-04-09 22:50:22 +0300

    Add files via upload

---
+++ main1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (void)
+{
+	int m,n;
+	printf("________________________________________________________________________________\n");
+	printf ("Choose your preffered dimensions\n");
+	printf ("Choose the LINE\n");
+	scanf ("%d", &m);
+	printf ("Choose the COLUMN\n");
+	scanf ("%d", &n);
+	printf("________________________________________________________________________________\n");
+	printf ("Choose the difficulty\n");
+	printf ("(e)EASY , (m)Medium , (h)HARD\n");
+	return 0;
+}
