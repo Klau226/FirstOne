0335ef57b11d051d9ebb62ce12278145231503a7
Klau226 <63421864+Klau226@users.noreply.github.com>
2020-04-11 13:52:50 +0300

    Add files via upload

--- main1.c
+++ main1.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 int calculate_percentages(int line , int column , int percent);
 
+typedef struct{ /* DEN KSERW AN EINAI SWSTO*/
+	int m;
+	int n;
+} dimensions;
+
+typedef struct { /* OUTE EDW */
+	char lvl;
+	dimensions array;
+	int obstacle_count;
+	int enemy_count;
+} final_board;
+
 int main (void)
 {
 	int m,n,obstacle_count,enemy_count;
 	char lvl;
+	int **array,i,j;
 	printf("________________________________________________________________________________\n");
 	printf ("Choose your preffered dimensions\n");
 	printf ("Choose the LINE\n");
@@ -13,53 +26,41 @@ int main (void)
 	printf ("Choose the COLUMN\n");
 	scanf ("%d", &n);
 	printf("________________________________________________________________________________\n");
+	array=(int**)malloc(m*sizeof(int*);   /* EKANA TON PINAKA DUNAMIKO */
+	for(i=0;i<m;i++){
+			array[i]=(int*)malloc(n*sizeof(int);
+	}
 	printf ("Choose the difficulty\n");
 	printf ("(e)EASY , (m)Medium , (h)HARD\n");
 	scanf("%c", &lvl);
-	while (lvl != ('e') && lvl != ('m') && lvl != ('h'))
-	{	
+	while (lvl != ('e') && lvl != ('m') && lvl != ('h')){	
 			printf ("PLEASE TYPE: (e)EASY , (m)Medium , (h)HARD\n");
-			scanf("%c", &lvl);
-			
+			scanf("%c", &lvl);	
 	}
-		
-
-	
-	if (lvl == 'e')
-	{
+	if (lvl == 'e'){
 		obstacle_count =calculate_percentages(m,n,5);
 		enemy_count =calculate_percentages(m,n,5);
 	}
-	else if (lvl == 'm')
-	{
+	else if (lvl == 'm'){
 		obstacle_count =calculate_percentages(m,n,5);
 		enemy_count =calculate_percentages(m,n,10);
 	}
-	else
-	{
+	else{
 		obstacle_count =calculate_percentages(m,n,10);
 		enemy_count =calculate_percentages(m,n,10);
 	}
 	printf ("\nThe obstacles will be :");	
 	printf("%d\n",obstacle_count);    	
 	printf ("\nThe enemys will be :");	
-	printf("%d\n",enemy_count);    	
-		
-	
+	printf("%d\n",enemy_count);    				
 	return 0;
 }
 
 int calculate_percentages(int line , int column , int percent)
 {
 	int x,s;
-	x=line*column;
-	
-	s=x*percent/100;
-	
-		
-	
-	
-	
+	x=line*column;	
+	s=x*percent/100;						
 	return s;
 }
 	
