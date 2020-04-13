893685ad89cbb6344c00265c02960615d0ae5c1a
Klau226 <63421864+Klau226@users.noreply.github.com>
2020-04-13 18:59:19 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -8,12 +8,17 @@ typedef struct{
 	int id;//Level 1, 2, 3 etc.
 }Level;
 typedef struct{
-	int board[10][10];
+	BoardElement* board[10][10];
 	int obstacles;
 	int enemies;
 	int x;
 	int y;
 }Board;
+typedef struct{
+	/* 0= Cell,1 = Knight,2 = Enemy,3 = Obstacle*/
+	int type;
+	char symbol;
+}BoardElement;
 typedef struct{
 	int symbol;
 }Enemy;
@@ -30,6 +35,7 @@ void end_level(Level* level);
 Board* create_board(int x, int y, int obstacles, int enemies);
 Enemy* create_enemy( char symbol ,Board* new_board );
 int insert_enemy(Board* board,Enemy* enemy);
+BoardElement* create_asterisk();
 
 int main (void)
 {
@@ -73,6 +79,11 @@ void init_level(Level* level, int x, int y, char selected_level){
 		enemy_count = calculate_percentage(x,y,10);
 	}
 	Board* new_board = create_board(x,y,obstacle_count,enemy_count);
+	for (int i = 0; i < x; i++) {
+        for (int j = 0; j < y; j++) {	
+           	printf("*");
+        }
+	/*
 	srand(time(NULL));
 	for (int i = 0; i < enemy_count; i++){
 		Enemy* new_enemy = create_enemy(symbol,new_board);
@@ -93,7 +104,7 @@ void init_level(Level* level, int x, int y, char selected_level){
 }
 Board* create_board(int x, int y, int obstacles, int enemies){
 	Board *new_board = (Board*)malloc(sizeof(Board));
-	int *tmp_arr = (int *)malloc(x * y * sizeof(int)); 
+	BoardElement *tmp_arr = (BoardElement *)malloc(x * y * sizeof(BoardElement)); 
 	memcpy(new_board->board, tmp_arr, sizeof(new_board->board));
 	new_board->obstacles = obstacles;
 	new_board->enemies = enemies;
@@ -108,6 +119,11 @@ Enemy* create_enemy( char symbol , Board* new_board){
 		new_enemy->symbol++;
 	}
 	return new_enemy;
+}
+BoardElement* create_asterisk(){
+	BoardElement *new_asterisk = (BoardElement*)malloc(sizeof(BoardElement));
+	new_asterisk->symbol = '*';
+	return new_asterisk;
 }
 int insert_enemy(Board* board,Enemy* enemy){
 	int randX = rand()%board->x;
